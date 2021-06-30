9fab76a64b55eadfee3ec1303ae26d1d4cabada1
yukarikariri <doggieholic3015@gmail.com>
2021-06-30 12:35:35 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get root to: 'rooms#index'
   # root to: 'messages#index'
   resources :users, only:[:edit, :update]
-  resources :rooms, only:[:new, :create]
+  resources :rooms, only:[:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
